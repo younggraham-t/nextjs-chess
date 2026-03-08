a71562e01e1967307121a4dfc3f280daa9a1c696
younggraham-t <younggraham.t@gmail.com>
2026-03-08 13:37:18 -0400

    experimenting with rust api endpoint integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 
+
 export const metadata: Metadata = {
   title: "Chess",
   description: "Chess web application",
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,25 @@ const nextConfig = {
 
     return config;
   },
+	rewrites: async () => {
+        const rewrites = {
+            afterFiles: [
+                // apply any of your existing rewrites here
+            ],
+            fallback: []
+        }
+
+        // dev only, this allows for local api calls to be proxied to
+        // api routes that use rust runtime
+        if (process.env.NODE_ENV === 'development') {
+            rewrites.fallback.push({
+                source: '/api/:path*',
+				destination: 'http://localhost:3001/api/:path*'
+            })
+        }
+
+        return rewrites
+    },
 };
 
 export default nextConfig;
